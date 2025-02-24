eeeb27c81551100557de308cf9dd272d7b93c630
Ivan Lepesii <lepesii.ivan@gmail.com>
2025-02-24 16:58:06 -0500

    feat: Adds axios instance (api)

---
+++ frontend/src/api/axiosInstance.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: "http://127.0.0.1:8000/",
+    timeout: 1000,
+    withCredentials: true,
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+    },
+});
+
+export default api;
